50f260bfa4d0fe7d785baa7bae66b46919deecc0
msgamesupor <63457083+msgamesupor@users.noreply.github.com>
2023-01-09 22:59:09 +0700

    Create Task1v1.java

---
+++ lesson5/Task1v1.java
@@ -0,0 +1,11 @@
+package lesson5;
+/* Задача 1:
+Создать массив char, заполненный буквами своего имени в верном порядке. Используя этот массив, вывести свое имя в консоль.
+Вариант 1: не используя переменную типа String; */
+
+public class Task1v1 {
+    public static void main(String[] args) {
+   char[] array = new char[]{'М','а','к','с','и', 'м'};
+        System.out.println(array);
+    }
+}
